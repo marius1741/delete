d83023a312681d1ed7aaf2d81beead874ce0b7b0
Petric Maria-Teodora <teodora.petric@fortech.ro>
2020-10-30 14:48:29 +0200

    added generic service

---
+++ delete.Business/ServiceImplementations/Service.cs
@@ -0,0 +1,50 @@
+﻿using delete.Business.ServiceInterfaces;
+using delete.Repository.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delete.Business.ServiceImplementations
+{
+    internal class Service<TEntity> : IService<TEntity> where TEntity : class
+    {
+        private readonly IRepository<TEntity> repository;
+        public Service(IRepository<TEntity> repositoryInjection)
+        {
+            repository = repositoryInjection;
+        }
+        public TEntity Add(TEntity entry)
+        {
+            var returnValue = repository.Add(entry);
+            repository.SaveChanges();
+
+            return returnValue;
+        }
+
+        public void Delete(int key)
+        {
+            repository.Delete(key);
+            repository.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return repository.Get().Done();
+        }
+
+        public TEntity Get(int key)
+        {
+            return repository.Get(key);
+        }
+
+        public TEntity Update(TEntity entry)
+        {
+            var value = repository.Update(entry);
+            repository.SaveChanges();
+
+            return value;
+        }
+    }
+}
---
+++ delete.Business/ServiceInterfaces/IService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delete.Business.ServiceInterfaces
+{
+    public interface IService<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get();
+        TEntity Get(int key);
+        TEntity Add(TEntity entry);
+        TEntity Update(TEntity entry);
+        void Delete(int key);
+    }
+}
