599db8f7ce3087d8beadcdf453b4a32db73e9049
Petric Maria-Teodora <teodora.petric@fortech.ro>
2020-11-03 12:11:44 +0200

    added delete service

--- delete.Business/ServiceImplementations/DeleteService.cs
+++ delete.Business/ServiceImplementations/DeleteService.cs
@@ -9,85 +9,24 @@ using System.Linq;
 
 namespace delete.Business.ServiceImplementations
 {
-    internal class DeleteService : IDeleteService
+    internal class DeleteService : Service<Delete>, IDeleteService
     {
-        private readonly IDeleteRepository deleteRepository;
-        private readonly IMapper mapper;
-        public DeleteService(IDeleteRepository deleteRepositoryInjection, IMapper mapperInjection)
+        private readonly IRepository<Delete> _deleteRepository;
+        private readonly IMapper _mapper;
+        public DeleteService(IRepository<Delete> deleteRepositoryInjection, IMapper mapperInjection) : base (deleteRepositoryInjection, mapperInjection) 
         {
-            deleteRepository = deleteRepositoryInjection;
-            mapper = mapperInjection;
+            _deleteRepository = deleteRepositoryInjection;
+            _mapper = mapperInjection;
         }
+
         public ServiceDataResponse<DeleteDTO> Add(DeleteDTO entry)
         {
-            var returnValue = new ServiceDataResponse<DeleteDTO>();
-            try
-            {
-                returnValue.Data = mapper.Map<DeleteDTO>(deleteRepository.Add(mapper.Map<Delete>(entry)));
-                deleteRepository.SaveChanges();
-                returnValue.Type = ResponseType.Success;
-            }
-            catch(Exception ex)
-            {
-                returnValue.Type = ResponseType.ServerError;
-                returnValue.Message = ex.Message;
-            }
-            return returnValue;
-        }
-        public ServiceResponse Delete(int id)
-        {
-            ServiceResponse hasSucceded = new ServiceResponse();
-            try
-            {
-                deleteRepository.Delete(id);
-                hasSucceded.Type = ResponseType.Success;
-            }
-            catch (Exception ex)
-            {
-                hasSucceded.Type = ResponseType.ServerError;
-                hasSucceded.Message = ex.Message;
-            }
-            return hasSucceded;
-        }
-        public ServiceDataResponse<Delete> Get(int id)
-        {
-            //return deleteRepository.Get().Where(s => s.OtherInfo.Contains(id.ToString())).Done().FirstOrDefault();
-            //return deleteRepository.Get().Where(s => s.Id == id).Done().FirstOrDefault();
-
-            var returnValue = new ServiceDataResponse<Delete>();
-            returnValue.Data = deleteRepository.Get(id);
-            returnValue.Type = returnValue.Data == null ? ResponseType.NotFound : ResponseType.Success;
-            returnValue.Message = returnValue.Data == null ? "Object not found" : string.Empty;
-
-            return returnValue;
-        }
-
-        public ServiceDataResponse<IEnumerable<Delete>> Get()
-        {
-            var returnValue = new ServiceDataResponse<IEnumerable<Delete>>();
-            returnValue.Data = deleteRepository.Get().Done();
-            returnValue.Type = returnValue.Data == null ? ResponseType.NotFound : ResponseType.Success;
-            returnValue.Message = returnValue.Data == null ? "Object not found" : string.Empty;
-
-            return returnValue;
+            throw new NotImplementedException();
         }
 
         public ServiceDataResponse<DeleteDTO> Update(DeleteDTO entry)
         {
-            var returnValue = new ServiceDataResponse<DeleteDTO>();
-            try
-            {
-                returnValue.Data = mapper.Map<DeleteDTO>(deleteRepository.Update(mapper.Map<Delete>(entry)));
-                deleteRepository.SaveChanges();
-                returnValue.Type = ResponseType.Success;
-            }
-            catch (Exception e)
-            {
-                returnValue.Type = ResponseType.ServerError;
-                returnValue.Message = e.Message;
-            }
-
-            return returnValue;
+            throw new NotImplementedException();
         }
     }
 }
